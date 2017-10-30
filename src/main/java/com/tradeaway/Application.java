dbd9d235bf21c46cf8547018921ae0e441ff391a
Sujith <sujiths@thoughtworks.com>
2017-10-30 18:11:09 +0530

    fixed CORS issue

--- src/main/java/com/tradeaway/Application.java
+++ src/main/java/com/tradeaway/Application.java
@@ -2,6 +2,11 @@ package com.tradeaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +14,19 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.addExposedHeader("Authorization");
+		config.addExposedHeader("Content-Type");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
 
 }
