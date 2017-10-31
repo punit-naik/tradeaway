69a37f57e8ebc168c5bbb3aed0beb98a8c2ba187
ajayvk <ajayvk@in-ajayvk.local>
2017-10-31 12:03:52 +0530

    [Ajay/ Naveen] Enable CORS

--- src/main/java/com/tradeaway/login/SecurityConfiguration.java
+++ src/main/java/com/tradeaway/login/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.tradeaway.login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,6 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .antMatchers("/", "/home").permitAll()
                     .anyRequest().authenticated().and().
                 formLogin().loginPage("/login").permitAll().and().
