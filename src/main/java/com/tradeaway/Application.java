1178d7e83ab9d615dc32be5a3e138849683d8d7d
punit <punit@in-punit.local>
2017-10-31 12:48:10 +0530

    :tada: :gift: [Punit] Enabled Swagger2 for all REST end-points

--- src/main/java/com/tradeaway/Application.java
+++ src/main/java/com/tradeaway/Application.java
@@ -1,14 +1,20 @@
 package com.tradeaway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableSwagger2
+@Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
