d84f4f1fc1ae48f540ec000d8f61c3cc5c463811
ajayvk <ajayvk@in-ajayvk.local>
2017-10-31 11:57:23 +0530

    [Ajay/ Naveen] Login Using Spring Security - form login - Fix missed files

--- src/main/java/com/tradeaway/login/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/com/tradeaway/login/CustomBasicAuthenticationEntryPoint.java
@@ -1,4 +1,20 @@
 package com.tradeaway.login;
 
-public class CustomBasicAuthenticationEntryPoint {
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Scope("prototype")
+public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(401,"Unauthorized");
+    }
 }
